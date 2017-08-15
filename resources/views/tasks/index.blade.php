bc4cd45f1ea821d7c216302d23887b390db8aa92
Sean Gallardo <sdvgallardo@gmail.com>
2017-08-15 12:21:19 -0500

    setting up framework for tasks app. pasted some formatting from previous project.

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,68 @@
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tasks!</title>
+</head>
+<body>
+    <ul>
+      @foreach ($tasks as $task)
+        <li>
+          <a href="/tasks/{{ $task->id }}">
+              {{ $task->body }}
+          </a>
+        </li>
+      @endforeach
+    </ul>
+</body>
+</html>
+-->
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta name="_token" content="{{ csrf_token() }}">
+    <title>Tasks!</title>
+
+    <!-- Load Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-narrow">
+        <h2>Task List</h2>
+        <button id="btn-add" name="btn-add" class="btn btn-primary btn-xs">Add New Task</button>
+        <div>
+
+            <!-- Table-to-load-the-data Part -->
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Task</th>
+                        <th>Date Added</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody id="tasks-list" name="tasks-list">
+                    @foreach ($tasks as $task)
+                    <tr id="task{{$task->id}}">
+                        <td> {{$task->id}} </td>
+                        <td> <a href="/tasks/{{ $task->id }}"> {{$task->body}} </a> </td>
+                        <td>{{$task->created_at}}</td>
+                        <td>
+                            <button class="btn btn-warning btn-xs btn-detail open-modal" value="{{$task->id}}">Edit</button>
+                            <button class="btn btn-danger btn-xs btn-delete delete-task" value="{{$task->id}}">Delete</button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+          </div>
+          </div>
+
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+          <script src="{{asset('js/ajax-crud.js')}}"></script>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,10 @@
                 </div>
 
                 <div class="links">
+                    <a href="/tasks/">Tasks</a>
+                    <a href="/about/">About</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
