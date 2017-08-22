4051dd48c76e686889e008bae773e77dccc7ab09
Sean Gallardo <sdvgallardo@gmail.com>
2017-08-22 18:01:56 -0500

    Last commit for the day

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -6,6 +6,7 @@
 
       <!-- Bootstrap core CSS -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+      <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"> -->
 
       <!-- Custom styles for this template -->
       <link href="/css/blog.css" rel="stylesheet">
@@ -38,22 +39,22 @@
           <tbody id="tasks-list" name="tasks-list">
             @foreach ($tasks as $task)
               @if ( $task->user->id == auth()->id() )
-
                 <tr id="task{{$task->id}}">
-                  <td> {{$task->id}} </td>
+                  <td> {{ $task->id }} </td>
                   <td> <a href="/tasks/{{ $task->id }}"> {{$task->body}} </a> </td>
                   <td>{{$task->created_at}}</td>
                   <td>{{$task->user->name }} </td>
                   <td> {{ $task->completed === 1 ? 'yes' : 'no' }}</td>
                   <td>
+                    <!-- <button class="btn btn-warning btn-xs btn-detail open-modal" value="{{$task->id}}">Edit</button> -->
                     @if ( $task->completed ) <a href="/tasks/complete/{{ $task->id }}"><button class="btn btn-warning btn-xs btn-detail" value="{{ $task->id }}">Incomplete</button></a>
-                    @else                    <a href="/tasks/complete/{{ $task->id }}"><button class="btn btn-warning btn-xs btn-detail" value="{{ $task->id }}">Complete</button></a>
+                    @else                    <a href="/tasks/complete/{{ $task->id }}"><button class="btn btn-success btn-xs btn-detail" value="{{ $task->id }}">Complete</button></a>
                     @endif
 
                     <a href="/tasks/delete/{{ $task->id }}"><button class="btn btn-danger btn-xs btn-delete" value="{{ $task->id }}">Delete</button></a>
                   </td>
                 </tr>
-                
+
               @endif
             @endforeach
           </tbody>
