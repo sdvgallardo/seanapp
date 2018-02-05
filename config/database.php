787fce967a14319379c9dfa6981d1a5f6692fb40
Sean Gallardo <sdvgallardo@gmail.com>
2018-02-05 15:14:56 -0600

    update database

--- config/database.php
+++ config/database.php
@@ -41,16 +41,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'seanapp'),
+            'username' => env('DB_USERNAME', 'seanapp'),
+            'password' => env('DB_PASSWORD', 'deadmeat7'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
