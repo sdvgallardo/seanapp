6f5d459f2687f35570ace75150049b2bffa5d92a
Sean Gallardo <sdvgallardo@gmail.com>
2018-02-05 16:39:27 -0600

    view changes

--- resources/views/layouts/blog/header.blade.php
+++ resources/views/layouts/blog/header.blade.php
@@ -1,6 +1,6 @@
 <div class="blog-header">
   <div class="container">
     <h1 class="blog-title">Sean's Test Blog</h1>
-    <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+    <p class="lead blog-description">An example blog.</p>
   </div>
 </div>
